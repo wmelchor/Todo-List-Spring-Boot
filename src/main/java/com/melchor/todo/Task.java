631968a4aa7b2aa9efbd5c3bf534665e2ceb3e02
William <williammelchor@gmail.com>
2023-08-25 22:01:47 -0400

    Added Task class and jakarta

---
+++ src/main/java/com/melchor/todo/Task.java
@@ -0,0 +1,54 @@
+package com.melchor.todo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id; 
+    private String task;
+    private boolean complete;
+
+    public Task(String task) {
+        this.task = task;
+        this.complete = false;
+    }
+
+    @Override
+    public String toString() {
+    return String.format(
+        "Task[id=%d, task='%s', complete='%b']",
+        id, task, complete);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+    
+    
+}
