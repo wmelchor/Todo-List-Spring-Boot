83eb0d8dd55de476c7fe9ad4dfc64992d1dc1f3c
William <williammelchor@gmail.com>
2023-08-30 16:35:30 -0400

    Added Spring Data JPA in XML file and TaskRepository interface

---
+++ src/main/java/com/melchor/todo/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.melchor.todo;
+
+import org.springframework.data.repository.CrudRepository;
+
+/*  CrudRepository is an interface from Spring Data JPA (In XML). It's designed to simplify the creation of data repositories in a Spring application. 
+ *  Thanks to extending CrudRepository, we get basic CRUD (Create, Read, Update, Delete) operations without having to write the implementation code for these operations,
+ *  we also get stuff like save, findById, findAll, and delete.
+*/
+public interface TaskRepository extends CrudRepository<Task, Long> {
+}
